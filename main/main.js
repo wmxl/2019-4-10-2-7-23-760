a0057cedcd488b2651680f9c86a4847c7c5437f1
wmxl <294036748@qq.com>
2019-05-28 22:30:13 +0800

    finished

--- main/main.js
+++ main/main.js
@@ -1,4 +1,13 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+	var p1,p2;
+	if(inputs.distance<=2){
+		p1=6
+	}else if(inputs.distance<=8){
+		p1=6+(inputs.distance-2)*0.8
+	}else{
+		p1=6+6*0.8+(inputs.distance-8)*1.2
+	}
+	p2 = inputs.parkTime * 0.25
+    return Math.round(p1+p2);
 };
